dd2ab00c34748d949d8db46b12bed58f996c68d5
Thomas McAndrew <tommcandrew@hotmail.com>
2020-06-30 23:36:32 +0100

    do about and contact pages

---
+++ src/pages/contact-us.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const ContactUs = () => {
+  return (
+    <Layout>
+      <div className="contactUs__content">
+        <h1 className="contactUs__title">Contact us</h1>
+        <div className="contactUs__inner">
+          <h2>Email: farbeyondlimit@gmail.com</h2>
+          <h2>Tel: (852) 9170 7930</h2>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default ContactUs;
