14b681b55eb5693ec87b064bab6731706bec83ee
Thomas McAndrew <tommcandrew@hotmail.com>
2020-07-01 14:04:36 +0100

    call joke api in function

--- functions/getJoke.js
+++ functions/getJoke.js
@@ -1,6 +1,17 @@
+const { default: Axios } = require("axios");
+
+const axios = require("axios");
+
 exports.handler = (event, context, callback) => {
-  callback(null, {
-    statusCode: 200,
-    body: "Hello, this is Thomas",
-  });
+  axios
+    .get("http://api.icndb.com/jokes/random1http://api.icndb.com/jokes/random")
+    .then((res) => {
+      callback(null, {
+        statusCode: 200,
+        body: res.data,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
